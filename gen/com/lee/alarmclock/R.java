13b59bbbfe2ebf8c9f64769b7aed78ec47884ca3
lee <609637057@qq.com>
2013-05-07 19:43:51 +0800

    更新了UI

--- gen/com/lee/alarmclock/R.java
+++ gen/com/lee/alarmclock/R.java
@@ -10,6 +10,10 @@ package com.lee.alarmclock;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bg=0x7f040001;
+        public static final int green=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +21,37 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int actionbar_shadow=0x7f020000;
+        public static final int add=0x7f020001;
+        public static final int addpres=0x7f020002;
+        public static final int bg=0x7f020003;
+        public static final int btnadd=0x7f020004;
+        public static final int btndelete=0x7f020005;
+        public static final int delete=0x7f020006;
+        public static final int deletepres=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int titel=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int addalarm=0x7f090002;
+        public static final int editalarm=0x7f090001;
+        public static final int titel=0x7f090000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
