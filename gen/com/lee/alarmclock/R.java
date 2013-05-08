9a03c9998f05f4b68242aa938f0a06217b821d49
lee <609637057@qq.com>
2013-05-08 09:16:02 +0800

    更新动画

--- gen/com/lee/alarmclock/R.java
+++ gen/com/lee/alarmclock/R.java
@@ -8,11 +8,18 @@
 package com.lee.alarmclock;
 
 public final class R {
+    public static final class anim {
+        public static final int enter=0x7f040000;
+        public static final int enteradd=0x7f040001;
+        public static final int exit=0x7f040002;
+        public static final int exitadd=0x7f040003;
+        public static final int zoom_enter=0x7f040004;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int bg=0x7f040001;
-        public static final int green=0x7f040000;
+        public static final int bg=0x7f050001;
+        public static final int green=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,8 +28,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int actionbar_shadow=0x7f020000;
@@ -37,31 +44,40 @@ public final class R {
         public static final int titel=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int addalarm=0x7f090002;
-        public static final int editalarm=0x7f090001;
-        public static final int titel=0x7f090000;
+        public static final int action_settings=0x7f0a0004;
+        public static final int addalarm=0x7f0a0003;
+        public static final int deletealarm=0x7f0a0002;
+        public static final int progressBar1=0x7f0a0000;
+        public static final int titel=0x7f0a0001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int addalarm=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
     }
     public static final class style {
+        public static final int ActionBar=0x7f080002;
+        public static final int AddEnter=0x7f080005;
+        public static final int AddExit=0x7f080006;
+        public static final int AnimationActivity=0x7f080003;
+        public static final int AnimationActivityadd=0x7f080004;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -75,10 +91,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/com/lee/alarmclock/MainActivity.java
+++ src/com/lee/alarmclock/MainActivity.java
@@ -1,17 +1,25 @@
 package com.lee.alarmclock;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
+	private ImageButton btnAdd,btnDelete;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE); 
 		setContentView(R.layout.main);
+		init();
 	}
 
 	@Override
@@ -21,6 +29,19 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	public void init() {
+		btnAdd=(ImageButton)findViewById(R.id.addalarm);
+		btnDelete=(ImageButton)findViewById(R.id.deletealarm);
+		
+		btnAdd.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(MainActivity.this, AddActivity.class);
+				startActivity(intent);
+				
+			}
+		});
 		
 	}
 }
